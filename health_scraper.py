9054d4a52e136ed8295d78df113b5ffd3d615c02
Wooran Jo <icfxg014@hanmail.net>
2022-06-26 02:32:24 +0900

    Enter

--- health_scraper.py
+++ health_scraper.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# token: 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
+url = "https://www.health.kr/researchInfo/herbalMedicine2.asp"
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get(url)
+driver.implicitly_wait(3)
+line_xpath = '/html/body/section/section/section/article[2]/div[1]/a' # 줄 선택
+line100_xpath = '/html/body/section/section/section/article[2]/div[1]/ul/li[6]/a' # 100줄 보기
+detail_xpath = '/html/body/section/section/section/article[2]/table/tbody/tr[{0}]/td[1]/a' # 2 to 101
 
 def scrape_name():
     find_all = soup.find_all('td')
@@ -26,43 +34,40 @@ def scrape_content():
     return dictionary
 
 def click_line100():
-    driver.find_element_by_xpath(line_xpath).click()
+    driver.find_element("xpath", line_xpath).click()
     driver.implicitly_wait(2)
-    driver.find_element_by_xpath(line100_xpath).click()
-    driver.implicitly_wait(2)
-
-url = "https://www.health.kr/researchInfo/herbalMedicine2.asp"
-driver = webdriver.Chrome('chromedriver.exe')
-driver.get(url)
-
-line_xpath = '/html/body/section/section/section/article[2]/div[1]/a' # 줄 선택
-line100_xpath = '/html/body/section/section/section/article[2]/div[1]/ul/li[6]/a' # 100줄 보기
-detail_xpath = '/html/body/section/section/section/article[2]/table/tbody/tr[{0}]/td[1]/a' # 2 to 101
-
-click_line100()
-
-df = pd.DataFrame()
-for i in range(2, 102):
-
-    driver.find_element_by_xpath(detail_xpath.format(i)).click()
-    driver.implicitly_wait(2)
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    dictionary = scrape_name()
-    content_dict = scrape_content()
-    dictionary.update(content_dict)
-
-    page_df = pd.DataFrame(dictionary, index=[0])
-    df = df.append(page_df, ignore_index=True)
-
-    driver.back()
+    driver.find_element("xpath", line100_xpath).click()
     driver.implicitly_wait(2)
 
-driver.close()
 
-for c in range(7, 12):
-  df[df.columns[c]] = df[df.columns[c]].apply(lambda x: str(x).split('\n'))
 
-pd.to_csv(df, encoding='utf-8-sig')
+try: 
+    click_line100()
+
+    df = pd.DataFrame()
+    for i in range(2, 102):
+
+        driver.find_element("xpath", detail_xpath.format(i)).click()
+        driver.implicitly_wait(2)
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        dictionary = scrape_name()
+        content_dict = scrape_content()
+        dictionary.update(content_dict)
+
+        page_df = pd.DataFrame(dictionary, index=[0])
+        df = df.append(page_df, ignore_index=True)
+
+        driver.back()
+        driver.implicitly_wait(2)
+finally:
+    driver.close()
+
+df.to_csv('health_scraped.csv', encoding='utf-8-sig')
+
+# version
+# after 4.3.0
+# find_element_by_xpath >> not working
+# changed: find_element("xpath", 'path')
